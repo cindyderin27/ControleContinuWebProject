2e975da773ef2bc8e3eb27d9caa5e6c1ef3051a3
cindyderin27 <61082408+cindyderin27@users.noreply.github.com>
2021-04-13 11:45:33 +0200

    Ajout de la base de donnée

---
+++ WebApiApplication/Controllers/ProprietaireController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiApplication.Models;
+
+namespace WebApiApplication.Controllers
+{
+    public class ProprietaireController : ApiController
+    {
+        ProprieteEntities db = new ProprieteEntities();
+
+        [HttpGet]
+        public IHttpActionResult GetListProprietaire(int index = 0, int taille = 10)
+        {
+            try
+            {
+                db.Configuration.ProxyCreationEnabled = false;
+                var proprietaire= db.Proprietaire.Include(nameof(Proprietaire)).OrderByDescending(x => x.Bien).                  
+                    Skip(index * taille).Take(taille).ToList();
+                return Ok();
+
+            }
+          
+
+            catch (Exception ex)
+
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+    }
+}
